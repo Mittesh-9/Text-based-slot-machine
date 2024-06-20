7706028dac76c2ae2ef77635e52a7c63f25d6189
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2024-06-20 11:48:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import random
+
+
 MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
 
 def deposit():
     while True:
